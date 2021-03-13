cc62500125f2503601e5f230c0d53d1dc9242228
Malakai <41552723+Whomever0@users.noreply.github.com>
2021-03-13 16:24:28 +0000

    commit

--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
 local base_url = "https://raw.githubusercontent.com/Whomever0/exploit-scripts/master/scripts/%s.lua"
 
+local exceptions = {":","/","."}
+
 local function hex(c)
+    if (table.find(exceptions, c)) then return c end
     return string.format("%%%02X", string.byte(c))
 end
 
@@ -8,7 +11,7 @@ local function urlencode(url)
     if url == nil then
         return url
     end
-    url = url:gsub("([^%w _%%%-%.~])", hex)
+    url = url:gsub("%W", hex)
     return url
 end
 
