2f184345a5cc1459d2e0678d6cded90a8b664e1f
Malakai <41552723+Whomever0@users.noreply.github.com>
2021-03-13 15:43:18 +0000

    commit

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+local base_url = "https://raw.githubusercontent.com/Whomever0/exploit-scripts/master/scripts/%s.lua"
 return function(n)
-    
+    return loadstring(game:HttpGet(string.format(base_url, n)))();
 end
