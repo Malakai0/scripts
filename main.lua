0c44a69cb7cefb23b777be4999afe4877cd481bf
Malakai <41552723+Whomever0@users.noreply.github.com>
2021-03-13 16:12:21 +0000

    commit

--- main.lua
+++ main.lua
@@ -1,15 +1,9 @@
 local base_url = "https://raw.githubusercontent.com/Whomever0/exploit-scripts/master/scripts/%s.lua"
-
-local function convert(c)
-    return string.format("%%%02X", string.byte(c))
-end
   
 local function urlencode(url)
     if url == nil then
-        return
+        return url
     end
-    url = url:gsub("\n", "\r\n")
-    url = url:gsub("([^%w _%%%-%.~])", convert)
     url = url:gsub(" ", "%20")
     return url
 end
