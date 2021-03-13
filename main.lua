696bb2822854064f1503fcd60dfbb8ebf53934c8
Malakai <41552723+Whomever0@users.noreply.github.com>
2021-03-13 15:51:49 +0000

    commit

--- main.lua
+++ main.lua
@@ -1,4 +1,20 @@
 local base_url = "https://raw.githubusercontent.com/Whomever0/exploit-scripts/master/scripts/%s.lua"
+
+local function convert(c)
+    return string.format("%%%02X", string.byte(c))
+end
+  
+local function urlencode(url)
+    if url == nil then
+        return
+    end
+    url = url:gsub("\n", "\r\n")
+    url = url:gsub("([^%w ])", convert)
+    url = url:gsub(" ", "+")
+    return url
+end
+
 return function(n)
-    return loadstring(game:HttpGet(string.format(base_url, n)))();
+    local url = urlencode(string.format(base_url, n));
+    return loadstring(game:HttpGet(url))();
 end
