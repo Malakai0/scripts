02a96756d5d26a70370064411c065c482709e605
Malakai <41552723+Whomever0@users.noreply.github.com>
2021-03-17 10:59:35 +0000

    hooker

---
+++ scripts/NamecallHooker.lua
@@ -0,0 +1,40 @@
+-- cause im lazy
+
+local hooker = {}
+local connections = {}
+
+function hooker.AddConnection(key, func)
+    connections[key] = func;
+    return {
+        Disconnect = function()
+            connections[key] = nil;
+        end
+    }
+end
+
+local mt = getrawmetatable(game);
+local namecall = mt.__namecall
+
+setreadonly(mt, false);
+
+mt.__namecall = function(self, ...)
+
+    for key,connection in next, connections do
+        if (type(connection) == 'function') then
+            local info = {
+                Player = game:GetService'Players'.LocalPlayer;
+                Method = getnamecallmethod();
+                CallingScript = getcallingscript();
+                Environment = getfenv(2);
+                Caller = self;
+                Args = {...};
+                Key = key;
+            }
+            connection(info);
+        end;
+    end
+
+    return namecall(self, ...)
+end
+
+return hooker;
