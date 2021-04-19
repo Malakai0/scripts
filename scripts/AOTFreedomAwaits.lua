4858aaed96ebc8516f316224ace71b6b59d515d5
Malakai <41552723+Whomever0@users.noreply.github.com>
2021-04-19 11:35:31 -0700

    Update AOTFreedomAwaits.lua

--- scripts/AOTFreedomAwaits.lua
+++ scripts/AOTFreedomAwaits.lua
@@ -44,7 +44,7 @@ local function CharAdded()
 	HUD:WaitForChild'Buttons'.ChildAdded:Connect(function(Child)
 		if getgenv().AntiGrab and Child.Name:lower() == 'letter' then
 			Child.Visible = false;
-            getgenv().ToDestroy[#getgenv().ToDestroy+1] = Child;
+			getgenv().ToDestroy[#getgenv().ToDestroy+1] = Child;
 		end
 	end)
 end
@@ -58,15 +58,15 @@ getgenv().CurCA = Player.CharacterAdded:Connect(CharAdded)
 
 local function Update()
 	ODM.idk = getgenv().DisableBackflip and (function() end) or ODM.idk; -- Remove stupid 'S' move.
-    ODM.Equipment.Gas = getgenv().UnlimitedGas and 10000 or ODM.Equipment.Gas;
-    ODM.Equipment.Blades = getgenv().UnlimitedBlades and 150 or ODM.Equipment.Blades;
-    ODM.Properties.Range = getgenv().InfiniteRange and 100000 or 300;
-    ODM.Properties.MaxSpeed = getgenv().MaxSpeed or 60;
+	ODM.Equipment.Gas = getgenv().UnlimitedGas and 10000 or ODM.Equipment.Gas;
+	ODM.Equipment.Blades = getgenv().UnlimitedBlades and 150 or ODM.Equipment.Blades;
+	ODM.Properties.Range = getgenv().InfiniteRange and 100000 or 300;
+	ODM.Properties.MaxSpeed = getgenv().MaxSpeed or 60;
 
-    for i,v in next, getgenv().ToDestroy do
-        v:Destroy()
-        getgenv().ToDestroy[i] = nil;
-    end
+	for i,v in next, getgenv().ToDestroy do
+		v:Destroy()
+		getgenv().ToDestroy[i] = nil;
+	end
 end
 
 local function Attack()
@@ -81,17 +81,17 @@ if (not getgenv().RanMTHook) then
 	setreadonly(MT,false)
 
 	MT.__namecall = function(self, ...)
-	    local Method = getnamecallmethod()
-	    local IsRemote = string.lower(Method) == 'fireserver'
-	    local IsExploitCall = ({...})[1] == RemoteExploitKey
-	    if self == KickEvent and IsRemote and getgenv().AntiKick then return end -- anti-kick
-	    if self == HurtEvent and IsRemote and getgenv().AntiHurt then return end; -- hurty hurty go away come again another day
-	    if self == AttackEvent and IsRemote and (not IsExploitCall) then
-	    	for i = 1, math.max((getgenv().Multiplier-1), 1) do
-	    		spawn(Attack)
-	    	end
-	    end
-	    return Namecall(self, ...);
+		local Method = getnamecallmethod()
+		local IsRemote = string.lower(Method) == 'fireserver'
+		local IsExploitCall = ({...})[1] == RemoteExploitKey
+		if self == KickEvent and IsRemote and getgenv().AntiKick then return end -- anti-kick
+		if self == HurtEvent and IsRemote and getgenv().AntiHurt then return end; -- hurty hurty go away come again another day
+		if self == AttackEvent and IsRemote and (not IsExploitCall) then
+			for i = 1, math.max((getgenv().Multiplier-1), 1) do
+				spawn(Attack)
+			end
+		end
+		return Namecall(self, ...);
 	end
 
 	if getgenv().AntiGrab then
